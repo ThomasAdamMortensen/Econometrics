b25ec0f7d2c70c53914acf9d2ab72241273ff8e9
Thomas Mortensen <123243170+ThomasAdamMortensen@users.noreply.github.com>
2024-06-01 13:55:22 +0200

    Add files via upload

--- Term paper/Term paper Econometrics.R
+++ Term paper/Term paper Econometrics.R
@@ -1,42 +1,22 @@
-# Okuns law? or philips law? https://www.investopedia.com/articles/economics/12/okuns-law.asp
-# Okun = GDP and unemployment inverse relationship
-# philips law unemployment and GDP inverse relationship
+library(tseries)
+library(dynlm)
+library(tsDyn)
+library(vars)
 
-# VARS are made to estimate relationship between multiple quantities over time,
-# meaning we can see how several quantities can be modeled using lags of itself and others.
-
-
-# One example of such case https://www.youtube.com/watch?v=UYRFYWNxX4c&ab_channel=JustinEloriaga
-
-# Download data with 2 variables, GDP and unemployment or inflation from Statistics sweden.
-
-# Then estimate VAR(P) of P lags and interpret coefs
-
-# Do tests to check if assumptions are met, what P to choose and more.
-# Such as KPSS and ADF and plotting/differencing to find I(D) for this data
-# Check for cointegration.
-
-# After models estimated, check residuals for normality/autocorrelation for assumptions and interpret
-
-# Either keep it in script format or Rmarkdown/quarto or stay to script.
-
-# Use latex to bind it all together in overleaf preferably.
-
-
-# If cointegrated, use levels do not diff
-
-# Konjunkturinstitutet, BNP GAP: https://prognos.konj.se/PxWeb/pxweb/sv/SenastePrognosen/SenastePrognosen__f24_resursutnyttjande/F2404.px/table/tableViewLayout2/?rxid=76600014-4460-47f5-8c63-680654f2935d
-
-setwd("~/Desktop/R Skript/Econometrics Econ applied 2b/Term paper")
+setwd("~/R_projects/Econometrics 2B Applied/Term paper") # Set wd to where files are
 
 rm(list = ls())
 
 df = read.csv("BNP_gap_1981Q1-2020Q4.csv", skip = 1, sep = ";", col.names = c("value","index","BNP_gap"))
 df$unemploy_perc = read.csv("16-64_Unemployed1981Q1-2020Q4.csv", skip = 1, sep = ",", header = FALSE)[,4]
 df = df[,2:4]
-plot(df[,-1]) # seems to have an inverse relationship as expected!
-cor(df[,-1]) # Negative correlation as expected
 
+par(mfrow = c(2,3))
+plot(df[,-1], main = "Scatterplot") # seems to have an inverse relationship as expected!
+plot(ts(df[2], start = c(1981, 1), frequency = 4), main = "GDP gap over time")
+plot(ts(df[3], start = c(1981, 1), frequency = 4), main = "Unemployment rate over time")
+plot(diff(ts(df[2], start = c(1981, 1), frequency = 4)), main = "Differenced BNP_gap")
+plot(diff(ts(df[3], start = c(1981, 1), frequency = 4)), main = "Differenced unemployment rate")
 adf.test(ts(df[,"BNP_gap"])) # Can't reject for BNP needs to be diffed
 adf.test(ts(df[,"unemploy_perc"]))# Can't reject for Unemploy_perc needs to be diffed
 
@@ -53,7 +33,43 @@ kpss.test(diff(ts(df[,"unemploy_perc"]))) # Can't reject, no evidence Unemployme
 
 ts_df = ts(df[,2:3], start = c(1981, 1), frequency = 4)
 
-# Papers on this for references, use about 2-4
-# https://www.ne.su.se/polopoly_fs/1.25832.1318427751!/menu/standard/file/Zeeshan_Arshad.pdf
-# http://www.diva-portal.se/smash/get/diva2:1134842/FULLTEXT01.pdf
-# https://www.diva-portal.org/smash/get/diva2:1700865/FULLTEXT01.pdf
+coint = dynlm(BNP_gap ~ unemploy_perc, data = ts_df)
+adf.test(coint$residuals) # evidence of cointegration which means we just fit a VAR in levels.
+VARselect(ts_df) # p = 9 according to AIC and other measures.
+
+okun_var = VAR(ts_df, p = 9)
+summary(okun_var)
+
+serial.test(okun_var, lags.pt = 18) # can reject null at 5%
+serial.test(okun_var, lags.pt = 27) # cannot reject null
+serial.test(okun_var, lags.pt = 36) # cannot reject null
+
+
+# We now test if the residuals are normally distributed by performing a multivariate 
+# Jarque-Bera test using normality.test()
+
+normality.test(okun_var)$jb.mul$JB # We reject the null of gaussian residuals with an extremely large test statistic.
+# Though this is fine as it is not a major assumption that makes the analysis useless or dubious if not fulfilled.
+
+
+Granger_BNP_U <- causality(okun_var, cause = "BNP_gap", vcov. = sandwich::vcovHC(okun_var))
+Granger_U_BNP <- causality(okun_var, cause = "unemploy_perc", vcov. = sandwich::vcovHC(okun_var))
+
+Granger_BNP_U$Granger # Evidence BNP gap granger causes unemployment rate
+Granger_U_BNP$Granger # No evidence of Unemployment rate granger causing BNP gap
+
+IRF_BNP_U <- irf(okun_var, impulse = "BNP_gap", response = "unemploy_perc", ortho = TRUE, boot = FALSE)
+IRF_U_BNP <- irf(okun_var, impulse = "unemploy_perc", response = "BNP_gap", ortho = TRUE, boot = FALSE)
+plot(IRF_BNP_U, main = "A shock in GDP gap") # A shock in BNP_gap leads to an instant decrease in Unemployment rate that keeps decreasing 
+# until 5 time periods after and then the effect of the shock weakens as time passes, with a small spike down again between 7:9.
+plot(IRF_U_BNP, main = "A shock in Unemployment Rate") # A shock in unemployment rate leads to no instant decrease in BNP gap and starts decreasing 
+# more every period until 4, then weakens as time passes, quite slowly though.
+
+FEVD = fevd(okun_var)
+
+round(FEVD$BNP_gap * 100, 1)
+round(FEVD$unemploy_perc * 100, 1)
+plot(FEVD) # For BNP gap it is mostly influenced by shocks in itself for all time periods, while unemployment rate 
+# is mostly influenced by shocks in itself 1 time period back,while any longer than that it is 
+# approximately equally affected by both BNP gap shocks and shocks in itself.
+
